21d9fc6342ea0b224c9f6244c8998a73b5766fc6
Tobias <tobias@roomtailors.de>
2020-05-31 14:35:57 +0200

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=_cYkkDdd912x.A
+export POSTGRES_HOST=udacity-microservice-dev.ccrbg2elm0l1.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=microservice-558257055904-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
